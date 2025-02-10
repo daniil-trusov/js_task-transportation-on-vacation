805d64f4b316040acd7729fa3d41a9027d9553c1
Daniil Trusov <daniiltrusov00@gmail.com>
2025-02-10 11:38:44 +0200

    create solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const shortTermDays = 3;
+  const shortTermDiscount = 20;
+
+  const longTermDays = 7;
+  const longTermDiscount = 50;
+
+  const rentForDay = 40;
+
+  const calculatedRent = rentForDay * days;
+
+  if (days >= longTermDays) {
+    return calculatedRent - longTermDiscount;
+  } else if (days >= shortTermDays) {
+    return calculatedRent - shortTermDiscount;
+  }
+
+  return calculatedRent;
 }
 
 module.exports = calculateRentalCost;
